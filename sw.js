21e31552c1689338398cf198f34f67b9856c4005
carlosnevesceasa <carlos.neves.ceasa@gmail.com>
2025-09-10 10:24:25 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,18 @@
-const CACHE_NAME = "etiquetas-cache-v1";
-const urlsToCache = [
-  "/",              // index.html
-  "/index.html",
-  "/manifest.json"
-];
+const CACHE_NAME = "etiquetas-cache-v2";
+const urlsToCache = [ "/", "/index.html", "/manifest.json" ];
 
-// Instala e faz cache dos arquivos
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", event=>{
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
 });
 
-// Ativa e limpa caches antigos
-self.addEventListener("activate", event => {
+self.addEventListener("activate", event=>{
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
+    caches.keys().then(keys=>
+      Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))
     )
   );
 });
 
-// Responde às requisições
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(
-      response => response || fetch(event.request)
-    )
-  );
+self.addEventListener("fetch", event=>{
+  event.respondWith(caches.match(event.request).then(res=>res||fetch(event.request)));
 });
