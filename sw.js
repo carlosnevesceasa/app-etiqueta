2729d1a8074aa30f8c6b94acaa5e457064af9bbd
carlosnevesceasa <carlos.neves.ceasa@gmail.com>
2025-11-07 12:14:04 -0300

    Update cache name from v5 to v6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "etiquetas-cache-v5";
+const CACHE_NAME = "etiquetas-cache-v6";
 const urlsToCache = [ "/", "/index.html", "/manifest.json" ];
 
 self.addEventListener("install", event=>{
